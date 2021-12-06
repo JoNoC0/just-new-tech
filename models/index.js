25719bb6e936f8e6b7c38775ff819d64bb72e1c8
JoNoC0 <jnguyeningco@gmail.com>
2021-12-06 17:47:09 -0600

    create api routes for post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+// User has many Posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// reverse association
+// Posts belonging to ONE USER
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
