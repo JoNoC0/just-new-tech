4a2e0d060ec7a2e08573fb20eaa83cc51ce89cc9
JoNoC0 <jnguyeningco@gmail.com>
2021-12-08 18:12:30 -0600

    refactor messy code

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 // User has many Posts
@@ -13,4 +14,36 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Vote.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Vote.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { 
+    User, 
+    Post,
+    Vote };
