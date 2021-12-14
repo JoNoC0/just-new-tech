a8ae295dcaa019cead988c3adc02295fabeb99df
JoNoC0 <jnguyeningco@gmail.com>
2021-12-13 21:04:46 -0600

    added style.css

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,8 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening ${PORT}`));
 });
