43498d1085116fce00534e73af53caff85ca779c
JoNoC0 <jnguyeningco@gmail.com>
2021-12-17 20:47:43 -0600

    corrected server.js

--- server.js
+++ server.js
@@ -2,22 +2,6 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const session = require('express-session');
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -35,7 +19,6 @@ const sess = {
   })
 };
 
-app.use(session(sess));
 
 const helpers = require('./utils/helpers');
 
@@ -43,6 +26,8 @@ const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
